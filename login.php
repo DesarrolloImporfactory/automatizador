be28ccb5a0623ed31a7e9cdfff9762dc8db0d7ab
Jeimy Jara <jeimyjara@hotmail.com>
2024-09-27 09:59:47 -0500

    Testing

--- login.php
+++ login.php
@@ -8,12 +8,12 @@ if (isset($_POST['email'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $sql = "SELECT * FROM usuarios WHERE email = '$email'";
+    $sql = "SELECT * FROM users WHERE email_users = '$email'";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
-        if (password_verify($password, $row['password'])) {
+        if (password_verify($password, $row['con_users'])) {
 
             $sql = "SELECT * FROM usuario_plataforma WHERE id_usuario = " . $row['id'];
             $result = $conn->query($sql);
