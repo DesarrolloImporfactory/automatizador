8249d37a805758a3e0c5e3bb4009b235fdc936bd
Kevin Becerra <k.becerra@imporfactorylatam.com>
2025-07-08 10:39:56 -0500

    widget_footer.php: se cambio fondo footer - Kevin

--- widget_footer.php
+++ widget_footer.php
@@ -1,6 +1,6 @@
 </div>
 <!-- Footer -->
-<footer class="bg-light text-center text-lg-start mt-auto">
+<footer class="text-white text-center text-lg-start mt-auto" style="background: #0f112b;">
     <div class="text-center p-3">
         &copy; 2025 <a href="https://new.imporsuitpro.com/">new.imporsuitpro.com</a> Todos los derechos reservados.
     </div>
